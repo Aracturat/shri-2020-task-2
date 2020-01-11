673649dba53ff5c0fcd3a46fc96b63a71b03609d
Nikolay Dozmorov <ndozmorov@gmail.com>
2020-01-11 17:39:00 +0700

    Add walk test and fix walk function

---
+++ src/walk.test.ts
@@ -0,0 +1,25 @@
+import * as parseJson from "json-to-ast";
+import { walk } from './walk';
+
+test('walk', () => {
+    const object = {
+        'block': 'name'
+    };
+
+    let ast = parseJson(JSON.stringify(object));
+
+    const nodeTypes: string[] = [];
+
+    walk(ast, (node, type) => nodeTypes.push(`${ node.type }:${type}`));
+
+    expect(nodeTypes).toEqual([
+        'Object:Enter',
+        'Property:Enter',
+        'Identifier:Enter',
+        'Identifier:Exit',
+        'Literal:Enter',
+        'Literal:Exit',
+        'Property:Exit',
+        'Object:Exit'
+    ]);
+});
--- src/walk.ts
+++ src/walk.ts
@@ -1,31 +1,27 @@
 import { AstEntity } from "json-to-ast";
 
 export function walk(node: AstEntity, func: (node: AstEntity, type: 'Enter' | 'Exit') => void) {
-    const queue = [node];
+    func(node, 'Enter');
 
-    for (const queueItem of queue) {
-        func(queueItem, 'Enter');
+    switch (node.type) {
+        case 'Array':
+        case 'Object':
+            if (node.children) {
+                node.children.forEach((item: AstEntity) => {
+                    walk(item, func);
+                });
+            }
 
-        switch (queueItem.type) {
-            case 'Array':
-            case 'Object':
-                if (queueItem.children) {
-                    queueItem.children.forEach((item: AstEntity) => {
-                        queue.push(item);
-                    });
-                }
+            break;
+        case 'Property':
+            walk(node.key, func);
+            walk(node.value, func);
 
-                break;
-            case 'Property':
-                queue.push(queueItem.key);
-                queue.push(queueItem.value);
-
-                break;
-            case 'Literal':
-            case 'Identifier':
-                break;
-        }
-
-        func(queueItem, 'Exit');
+            break;
+        case 'Literal':
+        case 'Identifier':
+            break;
     }
+
+    func(node, 'Exit');
 }
